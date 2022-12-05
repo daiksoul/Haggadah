ce3bb10b50ff1d103dc5468a235a04f9ecbae2de
DAIK <daiksoul@gmail.com>
2022-12-06 01:50:20 +0900

    proper login

--- lib/login.dart
+++ lib/login.dart
@@ -3,7 +3,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:haggah/data/firebase.dart';
+import 'package:haggah/data/localfile.dart';
 import 'package:haggah/main.dart';
+import 'package:haggah/store/storage.dart';
 import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
@@ -17,6 +20,14 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: SafeArea(
         child: ListView(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
@@ -24,61 +35,95 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 80.0),
             Column(
               children: <Widget>[
-                Consumer<ApplicationState>(
-                  builder: (context,state,_){
-                    return (state.isSignedIn)?Image.network(
-                      FirebaseAuth.instance.currentUser!.photoURL??"wee"
-                    ):const Icon(
-                      Icons.person,
-                      size: 60,
-                    );
-                  }
-                ),
+                Consumer<ApplicationState>(builder: (context, state, _) {
+                  return (state.isSignedIn)
+                      ? Image.network(
+                          FirebaseAuth.instance.currentUser!.photoURL ?? "wee")
+                      : const Icon(
+                          Icons.person,
+                          size: 60,
+                        );
+                }),
                 const SizedBox(height: 16.0),
                 Consumer<ApplicationState>(
-                  builder: (context,state,_){
-                    return (state.isSignedIn)?
-                        Text(FirebaseAuth.instance.currentUser!.displayName??"Nope")
-                        :const Text("로그인");
+                  builder: (context, state, _) {
+                    return (state.isSignedIn)
+                        ? Text(FirebaseAuth.instance.currentUser!.displayName ??
+                            "Nope")
+                        : const Text("로그인");
                   },
                 )
               ],
             ),
             const SizedBox(height: 120.0),
-            Consumer<ApplicationState>(
-              builder: (context, state, _) => IconLoginButton(
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.red.shade200),
-                    minimumSize:
-                        MaterialStateProperty.all(const Size.fromHeight(50)),
-                    padding: MaterialStateProperty.all(EdgeInsets.zero)),
-                text: "GOOGLE",
-                icon: const Icon(
-                  Icons.g_mobiledata_rounded,
-                  size: 40,
-                ),
-                onPressed: () async {
-                  UserCredential ud = await signInWithGoogle();
-                  if(ud.user!=null) {
-                    state.signIn();
-                    final user = FirebaseAuth.instance.currentUser!;
-                    final collection = FirebaseFirestore.instance.collection(
-                        "user");
-                    collection.where("uid", isEqualTo: user.uid).get().then((
-                        value) {
-                      if (value.docs.isEmpty) {
-                        collection.add({
-                          "uid": user.uid,
-                          "name": user.displayName,
-                          "email": user.email,
-                        });
-                      }
-                    });
-                    Navigator.pop(context);
-                  }
-                },
-              ),
+            Consumer2<ApplicationState, AppStorageState>(
+              builder: (context, state, store, _) => (!state.isSignedIn)
+                  ? IconLoginButton(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.green.shade200),
+                          minimumSize: MaterialStateProperty.all(
+                              const Size.fromHeight(50)),
+                          padding: MaterialStateProperty.all(EdgeInsets.zero)),
+                      text: "GOOGLE",
+                      icon: const Icon(
+                        Icons.g_mobiledata_rounded,
+                        size: 40,
+                      ),
+                      onPressed: () async {
+                        UserCredential ud = await signInWithGoogle();
+                        if (ud.user != null) {
+                          state.signIn();
+                          final user = FirebaseAuth.instance.currentUser!;
+                          final collection =
+                              FirebaseFirestore.instance.collection("users");
+                          collection
+                              .where("uid", isEqualTo: user.uid)
+                              .get()
+                              .then((value) {
+                            if (value.docs.isEmpty) {
+                              collection.doc(user.uid).set({
+                                "uid": user.uid,
+                                "name": user.displayName,
+                                "email": user.email,
+                              });
+                            }
+                          });
+                          readAllLocalCollection().then((list) {
+                            for (final val in list) {
+                              writeRemoteCollection(val);
+                            }
+                          });
+                          readAllRemoteCollection().then((list) {
+                            // print(list.length);
+                            for (final val in list) {
+                              // print(val.title);
+                              store.add(context, val);
+                            }
+                          });
+                          setState(() {});
+                        }
+                      },
+                    )
+                  : IconLoginButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.red.shade200),
+                        minimumSize: MaterialStateProperty.all(
+                            const Size.fromHeight(50)),
+                        padding: MaterialStateProperty.all(EdgeInsets.zero),
+                      ),
+                      text: "LOGOUT",
+                      icon: const Icon(
+                        Icons.logout,
+                        size: 40,
+                      ),
+                      onPressed: () {
+                        FirebaseAuth.instance.signOut();
+                        state.signOut();
+                        setState(() {});
+                      },
+                    ),
             ),
             const SizedBox(
               height: 20,
